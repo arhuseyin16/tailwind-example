215b2be1720e51d2bbe484e2fc382a69c83660b9
omerfrkarslan <omer.arslan@net-bt.com.tr>
2022-09-15 13:31:32 +0300

    apple component removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,14 +17,12 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { NzSelectModule } from "ng-zorro-antd/select";
 import { StoreModule } from "./store/store.module";
-import { AppleComponent } from './pages/apple/apple.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppleComponent
   ],
   imports: [
     BrowserModule,
