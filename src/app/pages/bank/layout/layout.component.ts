9f17e1beabb2a3217ed0859f4b9113396b3ce423
omerfrkarslan <omer.arslan@net-bt.com.tr>
2022-10-19 10:20:07 +0300

    table css düzeltildi. layout içerisinden translate işlemleri kaldırıldı.

--- src/app/pages/bank/layout/layout.component.ts
+++ src/app/pages/bank/layout/layout.component.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { TranslateService } from "@ngx-translate/core";
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { Store } from "@ngxs/store";
 import { SetIsCollapseAction } from "../../../store/sidebar/sidebar.action";
 
@@ -11,22 +10,11 @@ import { SetIsCollapseAction } from "../../../store/sidebar/sidebar.action";
 })
 export class LayoutComponent implements OnInit {
   isCollapsed = false;
-
-  constructor(
-    public translateService: TranslateService,
-    private store: Store
-  ) {
-    translateService.addLangs(['en', 'tr']);
-    translateService.setDefaultLang('en');
-  }
+  store = inject(Store);
 
   ngOnInit(): void {
     this.store.dispatch(new SetIsCollapseAction(this.isCollapsed));
   }
-  switchLang(lang: string) {
-    this.translateService.use(lang);
-  }
-
 
   changeCollapse() {
     this.isCollapsed = !this.isCollapsed;
