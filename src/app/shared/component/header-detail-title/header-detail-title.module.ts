d59a8ee3ead5106a3dfc994a4dcb10c6342cb409
huseyin.ar <huseyin.ar@net-bt.com.tr>
2022-10-28 14:11:53 +0300

    fix

---
+++ src/app/shared/component/header-detail-title/header-detail-title.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderDetailTitleComponent } from './header-detail-title.component';
+
+@NgModule({
+  declarations: [
+    HeaderDetailTitleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderDetailTitleModule { }
