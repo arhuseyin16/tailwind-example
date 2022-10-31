dce6ded81b5d132339215ad0643d2f5d926976b1
huseyin.ar <huseyin.ar@net-bt.com.tr>
2022-10-31 17:00:16 +0300

    döviz kurları ekranına başlandı

--- src/app/pages/settings/exchange/exchange.component.ts
+++ src/app/pages/settings/exchange/exchange.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Store} from "@ngxs/store";
-import {HeaderConfigClear} from "../../../store/header-config/header-config.action";
+import {HeaderConfigAction, HeaderConfigClear} from "../../../store/header-config/header-config.action";
+import {HeaderConfigModel} from "../../../models/header-config-model";
+import {FavoriteStateModel} from "../../../models/favorite-state.model";
+import {Router} from "@angular/router";
+import {FavoriteAction} from "../../../store/favorite/favorite.action";
 
 @Component({
   selector: 'app-exchange',
@@ -8,9 +12,25 @@ import {HeaderConfigClear} from "../../../store/header-config/header-config.acti
   styleUrls: ['./exchange.component.scss']
 })
 export class ExchangeComponent implements OnInit {
+  headerConfig: Array<HeaderConfigModel> = new Array<HeaderConfigModel>();
+  favoriteModel: FavoriteStateModel = new FavoriteStateModel();
 
-  constructor(private store: Store) {
-    this.store.dispatch(new HeaderConfigClear());
+  constructor(private store: Store,
+              private router: Router,
+  ) {
+    const dataObj = {
+      title: 'Döviz Kurları',
+    }
+    this.headerConfig.push({
+      component: () => import('../../../shared/component/header-title/header-title.component').then(it => it.HeaderTitleComponent),
+      dataObj: dataObj
+    });
+    this.store.dispatch(new HeaderConfigAction(this.headerConfig));
+    this.favoriteModel = {
+      name: 'sidebar.exchange',
+      url: this.router.url
+    }
+    this.store.dispatch(new FavoriteAction(this.favoriteModel));
   }
 
   ngOnInit(): void {
---
+++ src/app/shared/component/header-title/header-title.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderTitleComponent } from './header-title.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderTitleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeaderTitleModule { }
